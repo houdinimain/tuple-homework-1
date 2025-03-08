01b7bf31ffb407b90b31adb5dbb648c9621ca846
houdinimain <houdgondal@icloud.com>
2025-03-08 13:31:18 +0000

    Add files via upload

---
+++ hwtuples2.py
@@ -0,0 +1,30 @@
+group = input (" enter size of group")
+groupname = input("enter the group name")
+compday = input("What day is the competition")
+venue = input("What is the venue")
+medals = input("type of medals")
+
+tuple1 = (group,groupname,compday,venue,medals)
+
+
+
+print(tuple1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
